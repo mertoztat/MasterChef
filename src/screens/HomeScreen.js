fba4cee80776b5bf25fdf0fa71607acad26afc3e
Mert Öztat <mertoztat@gmail.com>
2023-09-02 22:18:37 +0300

    feat: homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,71 @@
-import { View, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Image, ScrollView, TextInput } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { BellIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import Categories from "../componets/Categories";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [activeCategory, setActiveCategory] = useState("Starter");
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+      <StatusBar style="dark" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 50 }}
+        className="space-y-6 pt-14"
+      >
+        <View className="mx-4 flex-row justify-between items-center mb-2">
+          <Image
+            style={{ height: hp(5), width: hp(5) }}
+            source={require("../../assets/images/avatar.png")}
+          />
+          <BellIcon size={hp(3)} color="gray" />
+        </View>
+
+        <View className="mx-4 space-y-2 mb-2">
+          <Text style={{ fontSize: hp(1.7) }} className="text-neutral-600">
+            Hello!
+          </Text>
+          <View>
+            <Text
+              style={{ fontSize: hp(3.8) }}
+              className="text-neutral-600 font-semibold"
+            >
+              Make your own food,
+            </Text>
+          </View>
+          <Text
+            style={{ fontSize: hp(3.8) }}
+            className="text-neutral-600 font-semibold"
+          >
+            stay at <Text className="text-amber-400">home</Text>
+          </Text>
+        </View>
+
+        <View className="mx-4 flex-row bg-black/5 items-center p-[4px] rounded-full ">
+          <TextInput
+            placeholder="Search any recipe"
+            placeholderTextColor={"gray"}
+            style={{ fontSize: hp(1.7) }}
+            className="flex-1 text-base mb-1 pl-3 tracking-wider"
+          />
+          <View className="bg-white rounded-full p-3">
+            <MagnifyingGlassIcon size={hp(2.5)} color="gray" strokeWidth={3} />
+          </View>
+        </View>
+
+        <View>
+          <Categories
+            activeCategory={activeCategory}
+            setActiveCategory={setActiveCategory}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
