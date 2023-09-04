50265dbd680be2e9aa5dea3cfd9eb7a95c94affa
Mert Öztat <mertoztat@gmail.com>
2023-09-04 15:42:08 +0300

    feat: create getRecipes service

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
+const GET_RECIPES = "https://www.themealdb.com/api/json/v1/1/filter.php?c=";
+
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 1000,
@@ -13,6 +15,15 @@ const api = axios.create({
 export const getAllCategories = async () => {
   try {
     const response = await api.get("/");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getRecipes = async (query) => {
+  try {
+    const response = await api.get(`${GET_RECIPES}${query}`);
     return response.data;
   } catch (error) {
     console.log(error);
