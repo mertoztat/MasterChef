a95d874437cedf7151cafdb0e4300169f0be77c9
Mert Öztat <mertoztat@gmail.com>
2023-09-05 22:05:04 +0300

    fix: fav button styling

--- src/componets/FavButton.js
+++ src/componets/FavButton.js
@@ -3,18 +3,13 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import { HeartIcon, StarIcon } from "react-native-heroicons/outline";
+import { StarIcon } from "react-native-heroicons/solid";
 
 export default function FavButton() {
   return (
     <View>
       <TouchableOpacity className="p-2 rounded-full mr-2 bg-white">
-        <StarIcon
-          size={hp(3.5)}
-          strokeWidth={2}
-          color="#fbbf24"
-          fill={"#fbbf24"}
-        />
+        <StarIcon size={hp(3.5)} strokeWidth={2} color="#fbbf24" />
       </TouchableOpacity>
     </View>
   );
