8b417fd1d412d8363bba95508e69d1c94b7b1773
Mert Öztat <mertoztat@gmail.com>
2023-09-06 16:38:10 +0300

    styling: adding animation welcome screen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -10,12 +10,14 @@ import { useNavigation } from "@react-navigation/core";
 
 export default function WelcomeScreen() {
   const innerCircle = useSharedValue(0);
+  const centerCircle = useSharedValue(0);
   const outerCircle = useSharedValue(0);
 
   const navigation = useNavigation();
 
   useEffect(() => {
     innerCircle.value = 0;
+    centerCircle.value = 0;
     outerCircle.value = 0;
     // inner circle animation
     setTimeout(
@@ -23,10 +25,16 @@ export default function WelcomeScreen() {
       100
     );
 
+    // center circle animation
+    setTimeout(
+      () => (centerCircle.value = withSpring(centerCircle.value + hp(5))),
+      300
+    );
+
     // outer circle animation
     setTimeout(
-      () => (outerCircle.value = withSpring(outerCircle.value + hp(5.5))),
-      300
+      () => (outerCircle.value = withSpring(outerCircle.value + hp(6))),
+      500
     );
 
     // navigate to home screen
@@ -36,35 +44,43 @@ export default function WelcomeScreen() {
   }, []);
 
   return (
-    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+    <View className="flex-1 justify-center items-center space-y-10 bg-violet-900">
       <StatusBar style="light" />
       <Animated.View
-        className="bg-white/20 rounded-full"
+        className="bg-violet-600 rounded-full"
         style={{ padding: outerCircle }}
       >
         <Animated.View
-          className="bg-white/20 rounded-full"
-          style={{ padding: innerCircle }}
+          className="bg-violet-500 rounded-full"
+          style={{ padding: centerCircle }}
         >
-          <Image
-            style={{ width: hp(20), height: hp(20) }}
-            source={require("../../assets/images/welcome.png")}
-          />
+          <Animated.View
+            className="bg-amber-400/90 rounded-full"
+            style={{ padding: innerCircle }}
+          >
+            <Image
+              style={{ width: hp(20), height: hp(20) }}
+              source={require("../../assets/images/chef.png")}
+            />
+          </Animated.View>
         </Animated.View>
       </Animated.View>
 
       <View className="flex items-center space-y-2">
         <Text
-          style={{ fontSize: hp(7) }}
-          className="font-bold text-white tracking-widest text-6xl"
+          style={{
+            fontSize: hp(7),
+            fontFamily: "Poppins_700Bold",
+          }}
+          className="text-white text-6xl  "
         >
-          Cook Book
+          Master<Text className="text-amber-400">Chef</Text>
         </Text>
         <Text
-          style={{ fontSize: hp(2) }}
-          className="font-medium text-white tracking-widest text-lg"
+          style={{ fontSize: hp(2), fontFamily: "Poppins_500Medium" }}
+          className="text-amber-400 tracking-widest text-xl"
         >
-          Enjoy your recipes
+          I hope you enjoy the recipe!
         </Text>
       </View>
     </View>
