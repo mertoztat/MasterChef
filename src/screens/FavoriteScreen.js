d4dc54634c953f8030d48049fa96431f69756844
mertoztat <mertoztat@gmail.com>
2023-09-08 16:33:44 +0300

    fix: favorite screens

--- src/screens/FavoriteScreen.js
+++ src/screens/FavoriteScreen.js
@@ -1,42 +1,53 @@
-import { View, Text, ScrollView, Image, FlatList } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
 import { StatusBar } from "expo-status-bar";
 import BackButton from "../componets/BackButton";
-import Favorites from "../componets/Favorites";
+import Favorites from "../componets/Favorites.js";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 export default function FavoriteScreen() {
   const favorites = useSelector((state) => state.favorites.favoriteList);
-  console.log("favorites", favorites);
 
   return (
-    <ScrollView
-      className="bg-white flex-1"
+    <View
+      className={`flex-1 h-40 ${
+        favorites.length === 0 ? "bg-violet-400" : "bg-white"
+      }`}
       showsVerticalScrollIndicator={false}
       contentContainerStyle={{ paddingBottom: 30 }}
     >
       <StatusBar style="light" />
-      <View className="mx-4 space-y-4">
+      <View className=" bg-violet-500 h-48 rounded-b-[50px] pt-20">
         <Text
-          className=" text-violet-500"
-          style={{ fontSize: hp(2.1), fontFamily: "Poppins_600SemiBold" }}
+          className=" text-amber-400 text-center"
+          style={{ fontSize: hp(4), fontFamily: "Poppins_600SemiBold" }}
         >
-          Recipes
+          FAVORITES
         </Text>
-        <BackButton />
-
-        <FlatList
-          nestedScrollEnabled={true}
-          scrollEnabled={false}
-          data={favorites}
-          renderItem={({ item, index }) => (
-            <Favorites item={item} index={index} />
-          )}
-          keyExtractor={(item) => item.idMeal.toString()}
-          numColumns={2}
-        />
       </View>
-    </ScrollView>
+      {favorites.length === 0 ? (
+        <View className="flex-1 items-center mt-40">
+          <View className="absolute mt-12">
+            <BackButton />
+          </View>
+          <Text
+            className="text-amber-400"
+            style={{ fontSize: hp(2.6), fontFamily: "Poppins_600SemiBold" }}
+          >
+            Favorite Recipes Not Founded
+          </Text>
+        </View>
+      ) : (
+        <View className="mt-12 rounded-t-3xl h-full">
+          <FlatList
+            data={favorites}
+            renderItem={({ item, index }) => (
+              <Favorites item={item} index={index} />
+            )}
+          />
+        </View>
+      )}
+    </View>
   );
 }
